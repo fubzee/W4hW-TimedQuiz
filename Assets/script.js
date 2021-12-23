e5319bf9e14d57baa278dbc5d8dfd39fb744452a
Sharon <fubzee@gmail.com>
2021-12-23 18:09:02 +1100

    Code changes to fix bugs and improve CSS

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,24 @@
 var timeEl = document.querySelector("#CountDownTimer");
-//var gameOver = 0000;
-var secondsLeft = {
-    hours: 0,
-    seconds: 6000
-}
-//    var countdown = document.querySelector("CountDown");                     
-//    var startBtn = document.querySelector("#Start");
-//    var backBtn = document.querySelector("Back");
-//    var resetBtn = document.querySelector("Clear")
-//    var button = document.querySelector("A");
-//    var button = document.querySelector("B");
-//    var button = document.querySelector("C");
-//    var button = document.querySelector("D");
-//    var question = document.querySelector("Question");
-var score = document.querySelector("ResultScore");
+//var gameOver = "";
+var secondsLeft = "";
+var selectedButton = "";
+var countdown = document.querySelector("#CountDownTimer");    
+var mainReturn = document.querySelector("#SectionOne"); 
+var goScores = document.querySelector("#SectionThree");                 
+var startBtn = document.querySelector("#start");
+var backBtn = document.querySelector("#back");
+var resetBtn = document.querySelector("#clear")
+var selectedButtonA = document.querySelector("#a");
+var selectedButtonB = document.querySelector("#b");
+var selectedButtonC = document.querySelector("#c");
+var selectedButtonD = document.querySelector("#d");
+var question = document.querySelector("#Question");
+var score = document.querySelector("#ResultScore");
+var gameMsg = document.querySelector("#result");
+var 
+
 var gameScore = 0;
-var gameMsg = " ";
+
 var scoreHistory = {
     initials: [],
     score:[]}; 
@@ -37,40 +40,38 @@ var question = {
 function startGame(){
     startGame.preventDefault;
     initials = prompt("Please enter your initials");
-    setTime();
-    stopQuiz();
+    secondsLeft = 60000;
+    reduceTime();
+    gameScore = 0;
+    startQuiz();
 }
 
-function setTime() {
-    secondsLeft = setInterval(countDown, secondsLeft, 1000);
+function reduceTime() {
+    console.log("reduce time", secondsLeft);
+    setInterval(countDown(secondsLeft),1000);
 }
 
 function countDown(secondsLeft){
-   
+        
+    console.log(secondsLeft)   ; 
     secondsLeft--;
+    console.log(secondsLeft)   ; 
     timeEl.textContent = secondsLeft;  
     document.getElementById("CountDownTimer").style.display = "block";
     document.getElementById("CountDownTimer").innerHTML = timeEl.textContent;
-    do {
-      startQuiz();}    
-    while (secondsLeft > 0);
-    stopQuiz();
+    if(secondsLeft < 0001){
+        clearInterval;
+        stopQuiz();
+    }    
 }
-function startQuiz(){
-      
-    loadQuestion(); 
     
-    selectedButton = false;
-    selectedButton = document.getElementById("a").addEventListener("click",checkResult(selectedButton)); 
-    selectedButton = document.getElementById("b").addEventListener("click",checkResult(selectedButton));
-    selectedButton = document.getElementById("c").addEventListener("click",checkResult(selectedButton));
-    selectedButton = document.getElementById("d").addEventListener("click",checkResult(selectedButton));
 
-    checkResult(selectedButton);
-    
+function startQuiz(){
+    loadQuestion(); 
 }
 
 function loadQuestion(){
+
     var NumQuestion = (question.number.length);
     setQuestion = question.number[Math.floor(Math.random() * NumQuestion)];
     console.log(setQuestion);
@@ -85,57 +86,109 @@ function loadQuestion(){
     console.log(question.optionD[setQuestion]);
     document.getElementById("d").innerHTML = question.optionD[setQuestion];
     console.log(question.correct[setQuestion]);
-}
-async function checkResult(selectedButton) {
     
-    if (selectedButton){
+}
+function checkResultA(selectedButtonA) {
+    
+    console.log("Selected Button",selectedButtonA);  
+    console.log("Selected Button",selectedButtonB);  
+    console.log("Selected Button",selectedButtonC);  
+    console.log("Selected Button",selectedButtonD);  
+    console.log("correct option",question.correct[setQuestion]);  //
+    selectedButton = question.optionA[setQuestion];
+    adjustTimeScore();
+    reduceTime()
+    startQuiz();
+}
+function checkResultB(selectedButtonB) {
+    
+    console.log("Selected Button",selectedButtonA);  
+    console.log("Selected Button",selectedButtonB);  
+    console.log("Selected Button",selectedButtonC);  
+    console.log("Selected Button",selectedButtonD);  
+    console.log("correct option",question.correct[setQuestion]);  //
+    selectedButton = question.optionB[setQuestion];
+    console.log(selectedButton);
+    adjustTimeScore();
+    reduceTime()
+    startQuiz();
+}
+
+function checkResultC(selectedButtonC) {
+    
+    console.log("Selected Button",selectedButtonA);  
+    console.log("Selected Button",selectedButtonB);  
+    console.log("Selected Button",selectedButtonC);  
+    console.log("Selected Button",selectedButtonD);  
+    console.log("correct option",question.correct[setQuestion]);  //
+    selectedButton = question.optionC[setQuestion];
+    console.log(selectedButton);
+    adjustTimeScore();
+    reduceTime()
+    startQuiz();
+}
+
+function checkResultD(selectedButtonD) {
+    
+    console.log("Selected Button",selectedButtonA);  
+    console.log("Selected Button",selectedButtonB);  
+    console.log("Selected Button",selectedButtonC);  
+    console.log("Selected Button",selectedButtonD);  
+    console.log("correct option",question.correct[setQuestion]);  //
+    selectedButton = question.optionD[setQuestion];
+    console.log(selectedButton);
+    adjustTimeScore();
+    reduceTime()
+    startQuiz();
+}
+
+function adjustTimeScore(){
+    console.log(selectedButton);
+    console.log(question.correct[setQuestion]);
+   
         if (selectedButton === question.correct[setQuestion]){
             gameScore = gameScore + 10;
-            gameMsg = "Well Done! That is the correct answer your score is " + gameScore;
+            gameMsg = "Well Done! That is the correct answer your score has increased by 10 points. Your total score = " + gameScore + " points";
         }
         else {
             secondsLeft = secondsLeft - 1000;
             gameMsg = "Oops that is incorrect, the timer has been reduced by 10 seconds";
         }
-        document.getElementById("result").innerHTML.textContent = gameMsg;
-    }
-    else {
-        await sleep(3000);
-        gameMsg = "You took to long to respond - the game will end";
-        secondsLeft = 0;
-    }
-}
-
-function sleep(ms) {
-    return new Promise(
-      resolve => setTimeout(resolve, ms)
-    );
+        document.getElementById("result").innerHTML = gameMsg;
+        console.log(gameMsg);
+    
+       
 }
 
 function stopQuiz(){
     addScore();
     gameMsg = "Game Over - Your final socre is " + gameScore;
     console.log(gameMsg);
-    document.getElementById("result").innerHTML.textContent = gameMsg;
-    document.getElementById("CountDownTimer").style.display = "none";
+    
+    document.getElementById("result").innerHTML = gameMsg;
+   
 }
 
 function addScore() {
     scoreHistory.initials=initials;
-    scoreHistory.score=score;    
+    scoreHistory.score=gameScore;    
     localStorage.setItem("Score History",scoreHistory);
     console.log(scoreHistory);
 }
 
 function getScore(){
 
-    var show = document.getElementById('scores');
-    if(show === "none"){
-        show.style.display = "flex";
-        show="flex";
-        return;
-   }
+    //var show = document.getElementById('scores');
+    //if(show === "none"){
+    //    show.style.display = "flex";
+    //    show="flex";
+   //     return;
+   //}
   
+
+
+
+
     
   /*localStorage.getItem(initials,score);*/
   console.log(scoreHistory);
@@ -150,25 +203,31 @@ console.log(scoreHistory);
 }
 
 function getResult(){
+    document.documentElement.scrollIntoView("SectionOne");
     document.getElementById("ResultInitials").innerHTML.textContent = initials;
-    addScore();
+    document.getElementById("ResultScore").innerHTML.textContent = score;
     getScore();
 }
 
 
 function gameReturn(){
-    var show = document.getElementById('scores');
-   
-   if (show === "flex"){
-          show.style.display = "none";
-          show="none";
-          return;
-   }
+        
+    document.documentElement.scrollIntoView("scores");
+    //document.getElementById("SectionThree").scrollTop = 0;
+//    var show = document.getElementById('scores');
+//   
+//   if (show === "flex"){
+//          show.style.display = "none";
+//          show="none";
+//          return;
+//   }
 }
 
-document.getElementById("start").addEventListener("click",startGame());
-   
-document.getElementById("clear").addEventListener("click",resetScore());
-
-document.getElementById("back").addEventListener("click",gameReturn());
-
+document.getElementById("start").addEventListener("click",function(){startGame()});
+document.getElementById("clear").addEventListener("click",function(){resetScore()});
+document.getElementById("back").addEventListener("click",function(){gameReturn()});
+document.getElementById("Trophy").addEventListener("click",function(){getResult()});
+document.getElementById("a").addEventListener("click",function(){checkResultA(selectedButtonA)}); 
+document.getElementById("b").addEventListener("click",function(){checkResultB(selectedButtonB)});
+document.getElementById("c").addEventListener("click",function(){checkResultC(selectedButtonC)});
+document.getElementById("d").addEventListener("click",function(){checkResultD(selectedButtonD)});
